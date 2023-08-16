a914317156a4ade3e21b37d8986c2a4845f8a005
Chirag Arora <chiragarora12371@gmail.com>
2023-08-16 09:48:53 +0530

    Add files via upload

---
+++ 3.map.js
@@ -0,0 +1,39 @@
+const p = [
+  {
+    id: 1,
+    name: "John Doe",
+    city: "New York",
+    state: "New York",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    city: "Los Angeles",
+    state: "California",
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: "Michael Johnson",
+    city: "Chicago",
+    state: "Illinois",
+    isActive: true,
+  },
+  {
+    id: 4,
+    name: "Emily Brown",
+    city: "Houston",
+    state: "Texas",
+    isActive: true,
+  },
+];
+
+const change = p.map((item) => {
+  if (item.isActive == "false") {
+    item.isActive = "NA";
+    return item;
+  }
+});
+
+console.log({ p, change });
