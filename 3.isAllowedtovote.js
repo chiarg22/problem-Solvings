c0f2f436d0db5eb503a0f0281927cbb32e201a84
Chirag Arora <chiragarora12371@gmail.com>
2023-08-17 09:30:53 +0530

    Update 3.isAllowedtovote.js

--- 3.isAllowedtovote.js
+++ 3.isAllowedtovote.js
@@ -2,4 +2,10 @@ function isallowedtovote(age) {
   return age >= 18 ? "True" : "False";
 }
 
-console.log(isallowedtovote(19));
+console.log(isallowedtovote(19)); // Giving us the string value
+
+function Validateage(age) {
+  return age >= 18;
+}
+
+console.log(Validateage(19)); // Giving us the boolean value
