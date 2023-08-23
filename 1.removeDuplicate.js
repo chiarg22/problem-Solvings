33f81c7f9f5dd365a036c5db9b4fe9986df73e24
Chirag Arora <chiragarora12371@gmail.com>
2023-08-23 06:19:18 +0530

    Add files via upload

---
+++ 1.removeDuplicate.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 0, 3, 2];
+
+function remove(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        arr[j] = undefined;
+      }
+    }
+  }
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] != undefined) {
+      console.log(arr[i]);
+    }
+  }
+}
+
+remove(arr);
+
+//This is a program to remove the Duplicate numbers from the array.
