7d701645e5cf004fc41a04ab5d2b987567a49424
Chirag Arora <chiragarora12371@gmail.com>
2023-08-23 06:50:04 +0530

    Add files via upload

---
+++ 3.swap.js
@@ -0,0 +1,10 @@
+//This is Day 2-Question 6.
+//Swapping of 2 numbers without third variable
+
+let a = 1;
+let b = 2;
+a = a + b; //3
+b = a - b; //1
+a = a - b; //2
+console.log("a:", a);
+console.log("b:", b);
