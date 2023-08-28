8cc4196d56928ebbc2a075495061c8b8da81ec87
Chirag Arora <chiragarora12371@gmail.com>
2023-08-29 00:08:03 +0530

    Update Intersection.js

--- Intersection.js
+++ Intersection.js
@@ -1,6 +1,6 @@
 //This is the program to find the union of 2 arrays
 //Considerign the arrays are  var arr1 = [ 1, 2, 4, 5, 6 ];
-//        var arr2 = [ 2, 3, 5, 7 ]; so the desired output should be arr3=arr1+arr2 => [1,2,4,5,6,3,5,7]
+//        var arr2 = [ 2, 3, 5, 7 ]; so the desired output should be [2,5];
 
 function Union(arr1, arr2) {
   x = arr1.length;
@@ -8,16 +8,16 @@ function Union(arr1, arr2) {
   for (i = 0; i < x; i++) {
     for (j = 0; j < y; j++) {
       if (arr2[j] === arr1[i]) {
-        arr1.splice(i, 1);
+        console.log(arr2[j]);
       }
     }
   }
-  console.log(arr1);
-  return arr1.concat(arr2); // Here we are using the concat method to add 2 arrays.
+  //   console.log(arr1);
+  //   return arr1.concat(arr2); // Here we are using the concat method to add 2 arrays.
 }
 
 var arr1 = [1, 2, 4, 5, 6];
 var arr2 = [2, 3, 5, 7];
-console.log(Union(arr1, arr2));
+Union(arr1, arr2);
 // arr = arr1 + arr2;
 // console.log(arr);
