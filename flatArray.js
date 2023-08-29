407e4d97e4424b9ee0975a24c84d6e896fe004c3
Chirag Arora <chiragarora12371@gmail.com>
2023-08-29 20:08:23 +0530

    Update flatArray.js

--- flatArray.js
+++ flatArray.js
@@ -1,6 +1,6 @@
-var searchMatrix = function (matrix) {
+var flatarray = function (matrix) {
   //   let k = 0;
-  empty = "";
+  empty = [];
   //   empty2 = [];
   m = matrix.length;
   var n = 0;
@@ -13,12 +13,17 @@ var searchMatrix = function (matrix) {
 
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      empty = empty + matrix[i][j];
+      empty.push(matrix[i][j]);
     }
   }
   return empty;
 };
 
 console.log(
-  searchMatrix([1, 2], [3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15])
+  searchMatrix([
+    [1, 2],
+    [3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14, 15],
+  ])
 );
