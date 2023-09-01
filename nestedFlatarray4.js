84bee0b85d16ea3251d9ffebd13c15cf6326989f
Chirag Arora <chiragarora12371@gmail.com>
2023-09-01 18:33:40 +0530

    Add files via upload

---
+++ nestedFlatarray4.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, [1, 2], [3, 4, [3, 4]], 5];
+function printarray(arr) {
+  return (result = arr + "");
+}
+
+console.log(printarray(arr));
+
+//This is approach provided by Romesh.
