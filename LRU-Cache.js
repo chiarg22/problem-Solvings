8dfd8198c81391da31c571c4d63c58b16707a7a9
Chirag Arora <chiragarora12371@gmail.com>
2023-10-06 20:41:52 +0530

    Update LRU-Cache.js

--- LRU-Cache.js
+++ LRU-Cache.js
@@ -1,4 +1,10 @@
 arr = [1, 2, 3, 4, 5, 7, 9];
+var LetsUnshift = function (array, item) {
+  for (var i = array.length - 1; i > 0; i--) {
+    array[i + 1] = array[i];
+  }
+  array[0] = item;
+};
 
 function LRU(arr, x) {
   if (arr.length > 0 && arr.length <= 8) {
@@ -6,11 +12,11 @@ function LRU(arr, x) {
       for (let i = 0; i < arr.length; i++) {
         if (arr[i] == x) {
           arr.splice(i, 1);
-          arr.unshift(x);
+          LetsUnshift(arr, x);
         }
       }
     } else {
-      arr.unshift(x);
+      LetsUnshift(arr, x);
     }
   }
   return arr;
